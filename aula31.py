2428a67cf97df431d25938c369d87fbfc68479dd
Andre Luiz Farias Antunes - PrestServ <andre.l.antunes.prestserv@petrobras.com.br>
2025-10-16 11:40:08 -0300

    Flags, is, is not e None

--- aula31.py
+++ aula31.py
@@ -4,15 +4,17 @@ None = Não valor
 is e is not = é ou não é (tipo, valor, identidade)
 id = Identidade
 """
+condicao = False
+passou_no_if = None
 
-v1 = 'a'
-v2 = 'b'
-print(id(v1))
-print(id(v2))
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+  print('Não faça algo')
 
-# condicao = False
 
-# if condicao:
-#   print('Faça algo')
-# else:
-#   print('Não faça algo')
+if passou_no_if is None:
+    print(passou_no_if, passou_no_if is None)
+else:
+    print(passou_no_if, passou_no_if is not None)
